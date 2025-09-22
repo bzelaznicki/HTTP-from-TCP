8594f62fa4dcdacc6a9f5e43c7f730f627123441
bzelaznicki <bartosz@zelaznicki.com>
2025-09-22 12:34:11 +0200

    now we're working with channels

--- main.go
+++ main.go
@@ -1,44 +1,22 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"os"
-	"strings"
 )
 
 func main() {
 	file, err := os.Open("messages.txt")
 
-	defer file.Close()
-	currentLine := ""
 	if err != nil {
 		log.Fatal("error: ", err)
 	}
 
-	for {
-		data := make([]byte, 8, 8)
+	ch := getLinesChannel(file)
 
-		n, err := file.Read(data)
-
-		if err != nil {
-			if currentLine != "" {
-				fmt.Printf("read: %s\n", currentLine)
-				currentLine = ""
-			}
-			if errors.Is(err, io.EOF) {
-				break
-			}
-		}
-		str := string(data[:n])
-		parts := strings.Split(str, "\n")
-		for i := 0; i < len(parts)-1; i++ {
-			fmt.Printf("read: %s%s\n", currentLine, parts[i])
-			currentLine = ""
-		}
-		currentLine += parts[len(parts)-1]
+	for line := range ch {
+		fmt.Printf("read: %s\n", line)
 	}
 
 }
--- reader.go
+++ reader.go
@@ -1,9 +1,44 @@
 package main
 
 import (
+	"errors"
 	"io"
+	"strings"
 )
 
-//func getLinesChannel(f io.ReadCloser) <-chan string {
+func getLinesChannel(file io.ReadCloser) <-chan string {
+	ch := make(chan string)
 
-//}
+	go func() {
+		defer file.Close()
+		defer close(ch)
+
+		currentLine := ""
+		for {
+			buf := make([]byte, 8)
+			n, err := file.Read(buf)
+
+			if n > 0 {
+				str := string(buf[:n])
+				parts := strings.Split(str, "\n")
+				for i := 0; i < len(parts)-1; i++ {
+					ch <- currentLine + parts[i]
+					currentLine = ""
+				}
+				currentLine += parts[len(parts)-1]
+			}
+
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					if currentLine != "" {
+						ch <- currentLine
+					}
+					return
+				}
+				return
+			}
+		}
+	}()
+
+	return ch
+}
