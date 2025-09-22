112edd346c8afd19197ebe2f4721349f1f530f5c
bzelaznicki <bartosz@zelaznicki.com>
2025-09-22 12:05:02 +0200

    reading full lines

--- main.go
+++ main.go
@@ -1,26 +1,44 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
 	file, err := os.Open("messages.txt")
 
+	defer file.Close()
+	currentLine := ""
 	if err != nil {
 		log.Fatal("error: ", err)
 	}
 
 	for {
-		data := make([]byte, 8)
+		data := make([]byte, 8, 8)
 
 		n, err := file.Read(data)
 
 		if err != nil {
-			break
+			if currentLine != "" {
+				fmt.Printf("read: %s\n", currentLine)
+				currentLine = ""
+			}
+			if errors.Is(err, io.EOF) {
+				break
+			}
 		}
-		fmt.Printf("read: %s\n", string(data[:n]))
+		str := string(data[:n])
+		parts := strings.Split(str, "\n")
+		for i := 0; i < len(parts)-1; i++ {
+			fmt.Printf("read: %s%s\n", currentLine, parts[i])
+			currentLine = ""
+		}
+		currentLine += parts[len(parts)-1]
 	}
+
 }
--- reader.go
+++ reader.go
@@ -1,1 +1,9 @@
 package main
+
+import (
+	"io"
+)
+
+//func getLinesChannel(f io.ReadCloser) <-chan string {
+
+//}
