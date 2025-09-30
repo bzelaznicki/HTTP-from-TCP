ea59dea4929d91dffff6719c8be5ad39cb02573c
bzelaznicki <bartosz@zelaznicki.com>
2025-09-30 16:55:06 +0200

    added headers

---
+++ internal/headers/headers.go
@@ -0,0 +1,45 @@
+package headers
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type Headers map[string]string
+
+const crlf = "\r\n"
+
+func NewHeaders() Headers {
+	return Headers{}
+}
+
+func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	i := bytes.Index(data, []byte(crlf))
+	if i == -1 {
+		return 0, false, nil
+	}
+	if i == 0 {
+		return 2, true, nil
+	}
+	name, value, err := parseHeader(data[:i])
+	if err != nil {
+		return 0, false, err
+	}
+	h[name] = value
+	return i + 2, false, nil
+}
+
+func parseHeader(fieldLine []byte) (string, string, error) {
+	parts := bytes.SplitN(fieldLine, []byte(":"), 2)
+	if len(parts) != 2 {
+		return "", "", fmt.Errorf("malformed header")
+	}
+
+	name := bytes.TrimLeft(parts[0], " \t")
+	if len(name) == 0 || bytes.ContainsAny(name, " \t") {
+		return "", "", fmt.Errorf("malformed field name")
+	}
+
+	value := bytes.TrimSpace(parts[1])
+	return string(name), string(value), nil
+}
